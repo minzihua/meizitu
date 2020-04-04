894cce083b12d35c58b9814b082994091e68896a
zihua <minzihua@foxmail.com>
2020-04-04 16:44:20 +0800

    下拉刷新完成

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'lib-flexible/flexible.js'
-import axios from 'axios'
-import { Lazyload } from 'vant';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "lib-flexible/flexible.js";
+import axios from "axios";
+import { Lazyload } from "vant";
 Vue.use(Lazyload);
-import { List } from 'vant';
+import { List } from "vant";
 Vue.use(List);
+/* 下拉刷新 */
+import { PullRefresh } from "vant";
+Vue.use(PullRefresh);
 
-axios.defaults.baseURL="https://gank.io/api/v2/";
-Vue.prototype.axios=axios;
+axios.defaults.baseURL = "https://gank.io/api/v2/";
+Vue.prototype.axios = axios;
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="con">
-    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <img class="img" v-for="(item,i) of list" :key="i" v-lazy="item.images[0]" />
-    </van-list>
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+      <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <img class="img" v-for="(item,i) of list" :key="i" v-lazy="item.images[0]" />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script>
 import { getMeizi } from "../assets/js/api/getMeizi";
+import { Toast } from "vant";
 export default {
   data() {
     return {
@@ -16,21 +19,42 @@ export default {
       count: 10,
       total_counts: 0,
       loading: false,
-      finished: false
+      finished: false,
+      isLoading: false
     };
   },
   methods: {
+    onRefresh() {
+      this.finished = false;
+      this.loading = true;
+      this.page = 1;
+      this.onLoad();
+    },
     onLoad() {
+      if (this.isLoading) {
+        this.list = [];
+        this.isLoading = false;
+        Toast("刷新成功");
+      }
+
       getMeizi(this.page, this.count).then(res => {
         this.list = this.list.concat(res.data.data);
         this.total_counts = res.data.total_counts;
+
         console.log(this.list);
         this.page += 1;
         this.loading = false;
         if (this.list.length == this.total_counts) {
           this.finished = true;
+          this.page = 1;
           console.log(this.page);
         }
+      });
+    },
+    myload() {
+      getMeizi(this.page, this.count).then(res => {
+        this.list = this.list.concat(res.data.data);
+        this.total_counts = res.data.total_counts;
       });
     }
   }
@@ -47,9 +71,9 @@ export default {
 }
 .con .img {
   width: 330px;
-  margin: 15px;
+  margin: 9px;
+  padding: 9px;
   border-radius: 15px;
-  border: 3px solid gainsboro;
   box-shadow: 0 5px 20px rgba(34, 25, 25, 0.4);
 }
 </style>
